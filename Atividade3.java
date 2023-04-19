f7cc016bfc8c7217b5f47e9d317f04a653fe33b9
Uilliam Santos de Aquino <126193812+uilliam-uill@users.noreply.github.com>
2023-04-19 11:39:53 -0300

    Add files via upload

---
+++ Atividade3.java
@@ -0,0 +1,60 @@
+package targetSistemas;
+
+public class Atividade3 {
+
+	public static void main(String[] args) {
+		/*LETRA A) int primeiroNumero = 1;
+			for(int i = 0; i<8; i++) {
+				System.out.println(primeiroNumero);
+				primeiroNumero+=2;
+			}*/
+		
+		/*LETRA B) int primeiroNumero = 2;
+		for(int i= 0; i< 10; i++) {
+			System.out.println(primeiroNumero);
+			primeiroNumero *= 2;
+		} */
+		
+		/* LETRA C) int soma = 0;
+		int verificar = 0;
+			for(int i = 0; i < 20; i++) {
+				verificar +=1 ;	
+				if(verificar % 2 != 0 ) {
+					soma += verificar;
+					System.out.println(soma);
+				}
+			} */
+		
+		/* LETRA D) for (int n = 1; n <= 5; n++) {
+		    int termo = (2*n + 2) * (2*n + 2);
+		    System.out.println(termo);
+		}	 */
+		
+		/* LETRA E) int n1 = 1, n2 = 1, n3, count = 2, maxCount = 10;
+		System.out.print(n1 + ", " + n2);
+
+		while (count < maxCount) {
+		    n3 = n1 + n2;
+		    System.out.print(", " + n3);
+		    n1 = n2;
+		    n2 = n3;
+		    count++;
+		}  */
+		
+       /*LETRA F) int[] sequencia = {2, 10, 12, 16, 17, 18, 19};
+        
+        System.out.print("Sequência: ");
+        for (int i = 0; i < sequencia.length; i++) {
+            System.out.print(sequencia[i] + " ");
+        }
+        
+        for (int i = 20; i <= 30; i++) {
+            System.out.print(i + " ");
+        } */	
+    }
+}
+
+
+	
+
+
